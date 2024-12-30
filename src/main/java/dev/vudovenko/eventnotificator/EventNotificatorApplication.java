6c56e2d5cb38c5573f2f78789518f6285aa91068
vudovenko <vladislav.udovenko0@yandex.ru>
2024-12-30 14:53:26 +0500

    git init

---
+++ src/main/java/dev/vudovenko/eventnotificator/EventNotificatorApplication.java
@@ -0,0 +1,13 @@
+package dev.vudovenko.eventnotificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNotificatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventNotificatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vudovenko/eventnotificator/EventNotificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vudovenko.eventnotificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
